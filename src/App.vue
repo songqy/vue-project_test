12a2eed4273d03526840414dbbb9db2ec1b48b75
songqiyan <songqy@maycur.com>
2020-01-16 19:22:29 +0800

    v2.0 init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/images/logo.png">
-    <transition name="fade" mode="out-in">
-    <router-view></router-view>
-    </transition>
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'App'
 }
 </script>
 
---
+++ src/components/Crawler.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="crawler">
+    <div>
+        <el-button type="primary">嘻嘻嘻</el-button>
+        <el-cascader
+            v-model="value"
+            :options="options"
+            :show-all-levels="false"
+            @change="handleChange"
+            :props="{ expandTrigger: 'hover' }"
+        />
+
+    </div>
+  </div>
+</template>
+
+<script>
+import cloneDeep from 'lodash/cloneDeep'
+import request from '../utils/request'
+
+const parseOptions = (files) => {
+  if (!files || files.length <= 0) {
+    return null
+  }
+  if (!files[0].children || files[0].children <= 0) {
+    return null
+  }
+  files.forEach(file => {
+    file.value = file.filePath
+    file.label = file.filePath
+    file.children = parseOptions(file.children)
+  })
+  return files
+}
+
+const getImgList = (files, value) => {
+  const file1 = files.find(file => file.filePath === value[0])
+  const file2 = file1.children.find(file => file.filePath === value[1])
+  if (value[2]) {
+    const file3 = file2.children.find(file => file.filePath === value[2])
+    return file3.children
+  }
+  return file2.children
+}
+
+const parseImgs = (imgs, value) => {
+  let prefix = `/${value[0]}/${value[1]}`
+  if (value[2]) {
+    prefix += `/${value[2]}`
+  }
+
+  let imgName = imgs.map(v => v.filePath)
+  imgName = imgName.filter(v => v.match(/img*/))
+
+  // TODO sort
+
+  return imgName.map(img => `${prefix}/${img}`)
+}
+
+export default {
+  name: 'Crawler',
+  options: [],
+  files: [],
+  imgs: [],
+
+  mounted: async function () {
+    const {content} = await request.get('/api_crawler/files')
+    this.files = cloneDeep(content.files)
+    this.options = parseOptions(content.files)
+    console.log(this.options)
+  },
+
+  methods: {
+    handleChange: function (value) {
+      console.log('handleChange')
+      console.log(value)
+
+      const imgs = getImgList(this.files, value)
+      this.imgs = parseImgs(imgs, value)
+    }
+  },
+
+  data () {
+    return {
+      value: [],
+      options: this.options,
+      files: this.files,
+      imgs: this.imgs
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
